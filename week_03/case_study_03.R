619c0ff9647d93f4568ee8d703e107be3a97569b
Joe Benz <joebenz14@gmail.com>
2021-09-22 17:44:17 -0400

    case study 3

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,31 @@
+#load packages
+library(ggplot2); library(gapminder); library(dplyr)
+
+#remove kuwait
+no_kuwait <- filter(gapminder, country != "Kuwait")
+
+#plot this 
+p1 <- ggplot(no_kuwait, aes(lifeExp, gdpPercap, color = continent, size = pop/10000))+
+             theme_bw()+
+             geom_point()+
+             facet_wrap(~year,nrow=1)+
+             scale_y_continuous(trans = "sqrt")+
+             labs(x="Life Expectancy", y="GDP per Capita")
+p1
+
+ grouped_no_kuwait <- group_by(no_kuwait, continent, year)
  
+ gapminder_continent <- summarize(grouped_no_kuwait, 
+                                  gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop), 
+                                  pop = sum(as.numeric(pop)))
+ 
+p2 <- ggplot(gapminder, aes(x = year, y = gdpPercap, size = pop/10000))+
+             geom_line()+
+             geom_point()+
+             geom_line(data=gapminder_continent, aes(gdpPercapweighted, pop))+
+             geom_point(data=gapminder_continent)+
+             facet_wrap(facets = gapminder_continent)+
+             theme_bw()+
+             labs(x = "Year", y = "GDP per Capita")
+
+p2
---
+++ week_03/case_study_3/case_study_3.R
@@ -0,0 +1,37 @@
+#load packages
+library(ggplot2); library(gapminder); library(dplyr)
+
+#remove kuwait
+no_kuwait <- filter(gapminder, country != "Kuwait")
+
+#plot this 
+p1 <- ggplot(no_kuwait, aes(lifeExp, gdpPercap, color = continent, size = pop/10000))+
+  theme_bw()+
+  geom_point()+
+  facet_wrap(~year,nrow=1)+
+  scale_y_continuous(trans = "sqrt")+
+  labs(x="Life Expectancy", y="GDP per Capita")
+p1
+
+ggsave("Plot1.png", width = 15, height = 5)
+
+grouped_no_kuwait <- group_by(no_kuwait, continent, year)
+
+gapminder_continent <- summarize(grouped_no_kuwait, 
+                                 gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop), 
+                                 pop = sum(as.numeric(pop))
+)
+
+p2 <- ggplot(no_kuwait, aes(x = year, y = gdpPercap))+
+  geom_line(aes(color=continent, group=country))+
+  geom_point(aes(color=continent))+
+  geom_line(gapminder_continent, mapping = aes(year, gdpPercapweighted))+
+  geom_point(gapminder_continent, mapping = aes(year, gdpPercapweighted, size = pop))+
+  theme_bw()+
+  facet_wrap(~continent,nrow=1)+
+  labs(x = "Year", y = "GDP per Capita")
+
+ggsave("Plot2.png", width = 15, height = 5)
+
+
+
