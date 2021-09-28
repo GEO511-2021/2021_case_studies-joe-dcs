0b95e46a20c5346cafcb5eaf77dae4554017a66f
Joe Benz <joebenz14@gmail.com>
2021-09-28 10:44:23 -0400

    case study 4 for real this time

--- week_04/case_study_4/case_study_4.R
+++ week_04/case_study_4/case_study_4.R
@@ -1,2 +1,18 @@
 library(tidyverse)
 library(nycflights13)
+
+arrange(flights, desc(distance))
+view(flights)
+#look at table to see code for furthest airport
+
+combined <- left_join(flights, airports, by=c("dest" = "faa"))
+#join tables 
+
+view(combined)
+#observe structure
+
+farthest_airport <- arrange(combined, desc(distance))%>%      
+  select(name)%>%
+  slice(1)%>%
+  as.character()
+#arrange new set by descending distance, select only the name, and slice off all but the first value 
